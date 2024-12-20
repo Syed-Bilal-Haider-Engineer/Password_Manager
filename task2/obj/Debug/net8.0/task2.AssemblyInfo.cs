df5cf83bbd24714d07e88afd63a682e54a393962
syed bilal haider <bilaldev151214@gmail.com>
2024-12-20 15:25:55 +0100

    Build angular app

--- task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67a8272c3ce346bf1e16c70a134ffbcfccf9769c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0742330b573595d07ed0c829429a191629855f72")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
